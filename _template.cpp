f7486725d1aefec5b5285dad8239abf1e6d3e495
tas9n <hw22a102@oecu.jp>
2025-11-15 23:07:59 +0900

    first commit

---
+++ _template.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <ios>
+#include <sstream>
+
+#include <concepts>
+
+#include <random>
+
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include <ranges>
+
+using namespace std;
+using namespace literals;
+using namespace ranges;
+
+using ll = long long;
+template<typename T>
+using Graph = vector<vector<T>>;
+
+#define all(obj) (obj).begin(), (obj).end()
+
+#define YESNO(flag) cout << (flag ? "YES" : "NO") << endl;
+#define YesNo(flag) cout << (flag ? "Yes" : "No") << endl;
+#define yesno(flag) cout << (flag ? "yes" : "no") << endl;
+
+#define Debug(x) cerr << "\t(line:" << __LINE__ << ") - " << #x << ": " << x << endl;
+#define VDebug(v) \
+cerr << "\t(line): " << __LINE__ << ")\n" << #v << ": { "; \
+for (auto& e : v) cerr << e << ", "; \
+cerr << "}" << endl;
+#define GDebug(g) \
+cerr << "\t(line): " << __LINE__ << ")\n" << #g << ": {\n"; \
+for (auto& v : g) {\
+    cerr << "\t{ ";\
+    for (auto& e : v) \
+        cerr << e << ", "; \
+    cerr << "}\n";\
+}\
+cerr << "}" << endl;
+
+
+#define INPUT(n) int n; cin >> n;
+#define INPUTS(n, v, type) vector<type> v(n); for(auto& e : v) cin >> e;
+#define GINPUTS(n, g, type) Graph<type> g(n, vector<type>(n)); for(auto& v : g) for(auto& e : v) cin >> e;
+
+#define LOOP(n) for(auto __loop_num = 0; __loop_num < n; __loop_num++)
+
+struct Point {
+    int x, y;
+
+    Point operator-(const Point& other) const {
+        return Point{ x - other.x, y - other.y };
+    }
+
+    bool operator==(const Point& other) const {
+        return (x == other.x) && (y == other.y);
+    }
+
+    bool operator<(const Point& other) const {
+        return (x == other.x) ? (y < other.y) : (x < other.x);
+    }
+
+    bool operator>(const Point& other) const {
+        return (x == other.x) ? (y > other.y) : (x > other.x);
+    }
+
+    int dist(const Point& other) const {
+        return abs(other.x - x) + abs(other.y - y);
+    }
+
+    friend ostream& operator<<(ostream& os, const Point& p) {
+        os << "{ " << p.x << ", " << p.y << " }";
+        return os;
+    }
+};
+
+int main() {
+    //TODO
+
+
+
+    return 0;
+}
